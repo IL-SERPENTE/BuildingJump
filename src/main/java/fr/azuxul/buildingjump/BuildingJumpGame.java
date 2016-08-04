45e95d96c6fd044a3d7834369e3f5185475bf702
azuxul <contact@azuxul.fr>
2016-08-04 20:54:19 +0200

    Add main inventory and chat for  different parts of the game

--- src/main/java/fr/azuxul/buildingjump/BuildingJumpGame.java
+++ src/main/java/fr/azuxul/buildingjump/BuildingJumpGame.java
@@ -1,11 +1,15 @@
 package fr.azuxul.buildingjump;
 
-import fr.azuxul.buildingjump.player.BuildingJumpPlayer;
+import fr.azuxul.buildingjump.player.PlayerBuildingJump;
 import net.samagames.api.SamaGamesAPI;
 import net.samagames.api.games.Game;
 import org.bukkit.Server;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Game instance of BuildingJump plugin
@@ -13,23 +17,44 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Azuxul
  * @version 1.0
  */
-public class BuildingJumpGame extends Game<BuildingJumpPlayer> {
+public class BuildingJumpGame extends Game<PlayerBuildingJump> {
 
     private final Plugin plugin;
     private final Server server;
     private final Configuration configuration;
+    private final Set<PlayerBuildingJump> playerInHub;
+    private final Set<PlayerBuildingJump> playerInBuildAndTest;
 
     public BuildingJumpGame(JavaPlugin javaPlugin) {
 
-        super("bulding_jump", "BuildingJump", "", BuildingJumpPlayer.class);
+        super("bulding_jump", "BuildingJump", "", PlayerBuildingJump.class);
 
         this.plugin = javaPlugin;
         this.server = javaPlugin.getServer();
         this.configuration = new Configuration(SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs());
 
+        this.playerInHub = new HashSet<>();
+        this.playerInBuildAndTest = new HashSet<>();
+
+    }
+
+    public Set<PlayerBuildingJump> getPlayerInHub() {
+        return playerInHub;
+    }
+
+    public Set<PlayerBuildingJump> getPlayerInBuildAndTest() {
+        return playerInBuildAndTest;
+    }
+
+    public Server getServer() {
+        return server;
     }
 
     public Configuration getConfiguration() {
         return configuration;
+    }
+
+    public PlayerBuildingJump getPlayer(Player player) {
+        return getPlayer(player.getUniqueId());
     }
 }
--- src/main/java/fr/azuxul/buildingjump/event/PlayerEvent.java
+++ src/main/java/fr/azuxul/buildingjump/event/PlayerEvent.java
@@ -1,12 +1,17 @@
 package fr.azuxul.buildingjump.event;
 
 import fr.azuxul.buildingjump.BuildingJumpGame;
-import fr.azuxul.buildingjump.player.BuildingJumpPlayer;
+import fr.azuxul.buildingjump.invetory.IInventory;
+import fr.azuxul.buildingjump.player.PlayerBuildingJump;
 import fr.azuxul.buildingjump.player.PlayerState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.InventoryHolder;
 
 /**
  * Player events
@@ -26,9 +31,54 @@ public class PlayerEvent implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
 
         Player player = event.getPlayer();
-        BuildingJumpPlayer buildingJumpPlayer = buildingJumpGame.getPlayer(player.getUniqueId());
+        PlayerBuildingJump playerBuildingJump = buildingJumpGame.getPlayer(player.getUniqueId());
 
-        buildingJumpPlayer.setState(PlayerState.HUB);
+        playerBuildingJump.setState(PlayerState.HUB);
         player.teleport(buildingJumpGame.getConfiguration().getHubLocation());
+    }
+
+    @EventHandler
+    public void onPlayerClick(InventoryClickEvent event) {
+
+        InventoryHolder inventoryHolder = event.getInventory().getHolder();
+
+        event.setCancelled(true);
+
+        if (event.getCurrentItem() != null && inventoryHolder != null && inventoryHolder instanceof IInventory) {
+
+            IInventory customInventory = (IInventory) inventoryHolder;
+
+            event.setCancelled(customInventory.click(event.getCurrentItem()));
+        }
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+
+        PlayerBuildingJump playerBuildingJump = buildingJumpGame.getPlayer(event.getPlayer());
+
+        if (PlayerState.HUB.equals(playerBuildingJump.getState())) {
+            for (PlayerBuildingJump player : buildingJumpGame.getPlayerInHub()) {
+                Player bukkitPlayer = player.getPlayerIfOnline();
+
+                if (bukkitPlayer != null)
+                    bukkitPlayer.sendMessage(event.getPlayer().getDisplayName() + ": " + event.getMessage());
+            }
+        } else if (PlayerState.TEST.equals(playerBuildingJump.getState()) || PlayerState.BUILD.equals(playerBuildingJump.getState())) {
+            for (PlayerBuildingJump player : buildingJumpGame.getPlayerInBuildAndTest()) {
+                Player bukkitPlayer = player.getPlayerIfOnline();
+
+                if (bukkitPlayer != null)
+                    bukkitPlayer.sendMessage(event.getPlayer().getDisplayName() + ": " + event.getMessage());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerPlaceBlock(BlockPlaceEvent event) {
+
+        PlayerBuildingJump playerBuildingJump = buildingJumpGame.getPlayer(event.getPlayer());
+
+        event.setCancelled(!PlayerState.BUILD.equals(playerBuildingJump.getState()));
     }
 }
