dfe4ca3639a0d4e41f0f3c866392cae2d729ae62
azuxul <contact@azuxul.fr>
2016-08-23 21:43:24 +0200

    Progress

--- src/main/java/fr/azuxul/buildingjump/BuildingJumpGame.java
+++ src/main/java/fr/azuxul/buildingjump/BuildingJumpGame.java
@@ -1,5 +1,7 @@
 package fr.azuxul.buildingjump;
 
+import fr.azuxul.buildingjump.jump.JumpAreaGenerator;
+import fr.azuxul.buildingjump.jump.JumpManager;
 import fr.azuxul.buildingjump.player.PlayerBuildingJump;
 import net.samagames.api.SamaGamesAPI;
 import net.samagames.api.games.Game;
@@ -8,8 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Game instance of BuildingJump plugin
@@ -22,6 +23,7 @@ public class BuildingJumpGame extends Game<PlayerBuildingJump> {
     private final Plugin plugin;
     private final Server server;
     private final Configuration configuration;
+    private final JumpManager jumpManager;
     private final Set<PlayerBuildingJump> playerInHub;
     private final Set<PlayerBuildingJump> playerInBuildAndTest;
 
@@ -32,6 +34,7 @@ public class BuildingJumpGame extends Game<PlayerBuildingJump> {
         this.plugin = javaPlugin;
         this.server = javaPlugin.getServer();
         this.configuration = new Configuration(SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs());
+        this.jumpManager = new JumpManager(this);
 
         this.playerInHub = new HashSet<>();
         this.playerInBuildAndTest = new HashSet<>();
