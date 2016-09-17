ae2af7fa4dfd948418afbed6988af6d59145e51e
azuxul <contact@azuxul.fr>
2016-09-17 22:34:08 +0200

    Cancel food level changing

--- src/main/java/fr/azuxul/buildingjump/event/PlayerEvent.java
+++ src/main/java/fr/azuxul/buildingjump/event/PlayerEvent.java
@@ -14,6 +14,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -147,5 +148,10 @@ public class PlayerEvent implements Listener {
         } else {
             event.setCancelled(true);
         }
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        event.setCancelled(true);
     }
 }
